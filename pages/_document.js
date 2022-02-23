c103b0a4deb91df905cba3b76b54007d77a4d6f2
Keith Taylor <KTSnowy@outlook.com>
2022-02-23 16:52:26 -0300

    Added Twitter banned and meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@ktsnowy_" />
+        <meta property="og:url" content="https://www.ktsnowy.com/" />
+        <meta property="og:title" content="KTSnowy" />
+        <meta property="og:description" content="Keith Taylor's Portfolio" />
+        <meta property="og:image" content="https://www.ktsnowy.com/banner.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
